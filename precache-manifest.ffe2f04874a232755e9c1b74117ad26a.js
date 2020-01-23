ec83a815370aaa1975a8930c296a2415d68355d9
MichelBitSavva <46565779+MichelBitSavva@users.noreply.github.com>
2020-01-23 23:30:58 +0300

    Updates

---
+++ precache-manifest.ffe2f04874a232755e9c1b74117ad26a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdb02db6eb5fc12dabac438abc00a5e",
+    "url": "/News_App/index.html"
+  },
+  {
+    "revision": "9346035666da5e484789",
+    "url": "/News_App/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "1abc79a34b5c0dab2068",
+    "url": "/News_App/static/css/main.48e0f554.chunk.css"
+  },
+  {
+    "revision": "9346035666da5e484789",
+    "url": "/News_App/static/js/2.e701aba2.chunk.js"
+  },
+  {
+    "revision": "002acf8ec12a70844c03454a043432b3",
+    "url": "/News_App/static/js/2.e701aba2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1abc79a34b5c0dab2068",
+    "url": "/News_App/static/js/main.0ac9af55.chunk.js"
+  },
+  {
+    "revision": "4363a1470313bf8deca9",
+    "url": "/News_App/static/js/runtime-main.48253ab1.js"
+  }
+]);
